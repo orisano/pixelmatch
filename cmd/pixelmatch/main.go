8dcb246b0af20464e72283340c89fa19f5df7079
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2019-02-27 20:18:44 +0900

    feat: initial commit

---
+++ cmd/pixelmatch/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/orisano/pixelmatch"
+	"github.com/pkg/errors"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix("pixelmatch: ")
+
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	threshold := flag.Float64("threshold", 0.1, "threshold")
+	dest := flag.String("dest", "-", "destination path")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) != 2 {
+		fmt.Println("Usage of pixelmatch [flags] image1 image2 :")
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	img1, err := openImage(args[0])
+	if err != nil {
+		return errors.Wrapf(err, "failed to open image(path=%v)", args[0])
+	}
+	img2, err := openImage(args[1])
+	if err != nil {
+		return errors.Wrapf(err, "failed to open image(path=%v)", args[1])
+	}
+
+	out, _, err := pixelmatch.MatchPixel(img1, img2, pixelmatch.Threshold(*threshold))
+	if err != nil {
+		return errors.Wrap(err, "failed to match pixel")
+	}
+
+	format := "png"
+	var w io.Writer
+	if *dest == "-" {
+		w = os.Stdout
+	} else {
+		switch ext := filepath.Ext(*dest); ext {
+		case ".png":
+			format = "png"
+		case ".jpeg", ".jpg":
+			format = "jpeg"
+		default:
+			return errors.Errorf("unsupported format: %v", ext)
+		}
+		f, err := os.Create(*dest)
+		if err != nil {
+			return errors.Wrap(err, "failed to create destination image")
+		}
+		defer f.Close()
+		w = f
+	}
+
+	var encErr error
+	switch format {
+	case "png":
+		encErr = png.Encode(w, out)
+	case "jpeg":
+		encErr = jpeg.Encode(w, out, nil)
+	}
+	if encErr != nil {
+		return errors.Wrap(encErr, "failed to encode")
+	}
+	return nil
+}
+
+func openImage(path string) (image.Image, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to open")
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to decode image")
+	}
+	return img, nil
+}
---
+++ pixelmatch.go
@@ -0,0 +1,82 @@
+package pixelmatch
+
+import (
+	"errors"
+	"image"
+	"image/color"
+)
+
+var ErrImageSizesNotMatch = errors.New("image sizes do not match")
+
+type MatchOptions struct {
+	threshold float64
+	includeAA bool
+}
+
+type MatchOption func(*MatchOptions)
+
+func Threshold(threshold float64) MatchOption {
+	return func(o *MatchOptions) {
+		o.threshold = threshold
+	}
+}
+
+func IncludeAntiAlias(o *MatchOptions) {
+	o.includeAA = true
+}
+
+func MatchPixel(a, b image.Image, opts ...MatchOption) (image.Image, int, error) {
+	options := MatchOptions{
+		threshold: 0.1,
+	}
+	for _, opt := range opts {
+		opt(&options)
+	}
+
+	if !a.Bounds().Eq(b.Bounds()) {
+		return nil, 0, ErrImageSizesNotMatch
+	}
+
+	maxDelta := 35215 * options.threshold * options.threshold
+	diff := 0
+	out := image.NewRGBA(a.Bounds())
+	rect := a.Bounds()
+	for y := rect.Min.Y; y < rect.Max.Y; y++ {
+		for x := rect.Min.X; x < rect.Max.X; x++ {
+			delta := subColor(a.At(x, y), b.At(x, y))
+			if delta > maxDelta {
+				out.SetRGBA(x, y, color.RGBA{R: 255, G: 0, B: 0, A: 255})
+				diff++
+			} else {
+				c := color.GrayModel.Convert(a.At(x, y)).(color.Gray)
+				c.Y = 255 - uint8(float64(255 - c.Y) * 0.1)
+				out.Set(x, y, c)
+			}
+		}
+	}
+	return out, diff, nil
+}
+
+func subColor(a, b color.Color) float64 {
+	y1, i1, q1 := blendColor(a)
+	y2, i2, q2 := blendColor(b)
+	return 0.5053*(y1-y2)*(y1-y2) + 0.299*(i1-i2)*(i1-i2) + 0.1957*(q1-q2)*(q1-q2)
+}
+
+func blendColor(c color.Color) (float64, float64, float64) {
+	r, g, b, a := c.RGBA()
+	r = r >> 8
+	g = g >> 8
+	b = b >> 8
+	a = a >> 8
+	if a < 255 {
+		x := float64(a) / 255
+		r = uint32(255 + float64(r-255)*x)
+		g = uint32(255 + float64(g-255)*x)
+		b = uint32(255 + float64(b-255)*x)
+	}
+	y := float64(r)*0.29889531 + float64(g)*0.58662247 + float64(b)*0.11448223
+	i := float64(r)*0.59597799 + float64(g)*0.27417610 + float64(b)*0.32180189
+	q := float64(r)*0.21147017 + float64(g)*0.52261711 + float64(b)*0.31114694
+	return y, i, q
+}
