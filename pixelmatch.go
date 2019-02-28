a0586483d6c006fc8dd7880c2297ea943854d895
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2019-02-28 17:17:54 +0900

    refactor: to original implementation

--- pixelmatch.go
+++ pixelmatch.go
@@ -55,7 +55,7 @@ func MatchPixel(a, b image.Image, opts ...MatchOption) (int, error) {
 	rect := a.Bounds()
 	for y := rect.Min.Y; y < rect.Max.Y; y++ {
 		for x := rect.Min.X; x < rect.Max.X; x++ {
-			delta := subColor(a.At(x, y), b.At(x, y))
+			delta := colorDelta(a.At(x, y), b.At(x, y), false)
 			if delta > maxDelta {
 				if out != nil {
 					out.SetRGBA(x, y, color.RGBA{R: 255, G: 0, B: 0, A: 255})
@@ -78,26 +78,45 @@ func MatchPixel(a, b image.Image, opts ...MatchOption) (int, error) {
 	return diff, nil
 }
 
-func subColor(a, b color.Color) float64 {
-	y1, i1, q1 := blendColor(a)
-	y2, i2, q2 := blendColor(b)
-	return 0.5053*(y1-y2)*(y1-y2) + 0.299*(i1-i2)*(i1-i2) + 0.1957*(q1-q2)*(q1-q2)
+func colorDelta(a, b color.Color, yOnly bool) float64 {
+	ca := color.RGBAModel.Convert(a).(color.RGBA)
+	cb := color.RGBAModel.Convert(b).(color.RGBA)
+	if ca.A == cb.A && ca.R == cb.R && ca.G == cb.G && ca.B == cb.B {
+		return 0
+	}
+	blendRGBA(&ca)
+	blendRGBA(&cb)
+
+	y := rgbaToY(&ca) - rgbaToY(&cb)
+	if yOnly {
+		return y
+	}
+	i := rgbaToI(&ca) - rgbaToI(&cb)
+	q := rgbaToQ(&ca) - rgbaToI(&cb)
+	return 0.5053*y*y + 0.299*i*i + 0.1957*q*q
 }
 
-func blendColor(c color.Color) (float64, float64, float64) {
-	r, g, b, a := c.RGBA()
-	r = r >> 8
-	g = g >> 8
-	b = b >> 8
-	a = a >> 8
-	if a < 255 {
-		x := float64(a) / 255
-		r = uint32(255 + float64(r-255)*x)
-		g = uint32(255 + float64(g-255)*x)
-		b = uint32(255 + float64(b-255)*x)
+func blendRGBA(rgba *color.RGBA) {
+	if rgba.A < 255 {
+		a := float64(rgba.A) / 255
+		rgba.R = blend(rgba.R, a)
+		rgba.G = blend(rgba.G, a)
+		rgba.B = blend(rgba.B, a)
 	}
-	y := float64(r)*0.29889531 + float64(g)*0.58662247 + float64(b)*0.11448223
-	i := float64(r)*0.59597799 + float64(g)*0.27417610 + float64(b)*0.32180189
-	q := float64(r)*0.21147017 + float64(g)*0.52261711 + float64(b)*0.31114694
-	return y, i, q
+}
+
+func blend(c uint8, a float64) uint8 {
+	return uint8(255 + float64(c-255)*a)
+}
+
+func rgbaToY(rgba *color.RGBA) float64 {
+	return float64(rgba.R)*0.29889531 + float64(rgba.G)*0.58662247 + float64(rgba.B)*0.11448223
+}
+
+func rgbaToI(rgba *color.RGBA) float64 {
+	return float64(rgba.R)*0.59597799 + float64(rgba.G)*0.27417610 + float64(rgba.B)*0.32180189
+}
+
+func rgbaToQ(rgba *color.RGBA) float64 {
+	return float64(rgba.R)*0.21147017 + float64(rgba.G)*0.52261711 + float64(rgba.B)*0.31114694
 }
