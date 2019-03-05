fc1cb5afa716fe610b9f65f8cc5cc1d27197f09a
Nao YONASHIRO <owan.orisano@gmail.com>
2019-03-05 10:42:46 +0900

    wip

--- pixelmatch.go
+++ pixelmatch.go
@@ -52,18 +52,29 @@ func MatchPixel(a, b image.Image, opts ...MatchOption) (int, error) {
 	maxDelta := 35215 * options.threshold * options.threshold
 	diff := 0
 
+	na := a.(*image.NRGBA)
+	nb := b.(*image.NRGBA)
+
+	var ca, cb color.RGBA
+
+	yellow := color.RGBA{R: 255, G: 255, A:255}
+	red := color.RGBA{R: 255, A: 255}
+
 	rect := a.Bounds()
 	for y := rect.Min.Y; y < rect.Max.Y; y++ {
 		for x := rect.Min.X; x < rect.Max.X; x++ {
-			delta := colorDelta(a.At(x, y), b.At(x, y), false)
+			fastAt(&ca, na, x, y)
+			fastAt(&cb, nb, x, y)
+
+			delta := colorDelta(&ca, &cb, false)
 			if delta > maxDelta {
-				if !options.includeAA && (isAntiAliased(a, b, x, y) || isAntiAliased(b, a, x, y)) {
+				if !options.includeAA && (isAntiAliased(na, nb, x, y) || isAntiAliased(nb, na, x, y)) {
 					if out != nil {
-						out.SetRGBA(x, y, color.RGBA{R: 255, G: 255, A: 255})
+						out.SetRGBA(x, y, yellow)
 					}
 				} else {
 					if out != nil {
-						out.SetRGBA(x, y, color.RGBA{R: 255, A: 255})
+						out.SetRGBA(x, y, red)
 					}
 					diff++
 				}
@@ -84,21 +95,19 @@ func MatchPixel(a, b image.Image, opts ...MatchOption) (int, error) {
 	return diff, nil
 }
 
-func colorDelta(a, b color.Color, yOnly bool) float64 {
-	ca := color.RGBAModel.Convert(a).(color.RGBA)
-	cb := color.RGBAModel.Convert(b).(color.RGBA)
-	if ca.A == cb.A && ca.R == cb.R && ca.G == cb.G && ca.B == cb.B {
+func colorDelta(a, b *color.RGBA, yOnly bool) float64 {
+	if a.A == b.A && a.R == b.R && a.G == b.G && a.B == b.B {
 		return 0
 	}
-	blendRGBA(&ca)
-	blendRGBA(&cb)
+	blendRGBA(a)
+	blendRGBA(b)
 
-	y := rgbaToY(&ca) - rgbaToY(&cb)
+	y := rgbaToY(a) - rgbaToY(b)
 	if yOnly {
 		return y
 	}
-	i := rgbaToI(&ca) - rgbaToI(&cb)
-	q := rgbaToQ(&ca) - rgbaToQ(&cb)
+	i := rgbaToI(a) - rgbaToI(b)
+	q := rgbaToQ(a) - rgbaToQ(b)
 	return 0.5053*y*y + 0.299*i*i + 0.1957*q*q
 }
 
@@ -127,7 +136,7 @@ func rgbaToQ(rgba *color.RGBA) float64 {
 	return float64(rgba.R)*0.21147017 - float64(rgba.G)*0.52261711 + float64(rgba.B)*0.31114694
 }
 
-func isAntiAliased(a, b image.Image, x, y int) bool {
+func isAntiAliased(a, b *image.NRGBA, x, y int) bool {
 	r := a.Bounds()
 	if onEdge(r, x, y) {
 		return false
@@ -138,7 +147,9 @@ func isAntiAliased(a, b image.Image, x, y int) bool {
 	max := float64(0)
 	maxX, maxY := -1, -1
 
-	c := a.At(x, y)
+	var c, nc color.RGBA
+	fastAt(&c, a, x, y)
+
 	zeroes := 0
 	for dy := -1; dy <= 1; dy++ {
 		for dx := -1; dx <= 1; dx++ {
@@ -147,7 +158,8 @@ func isAntiAliased(a, b image.Image, x, y int) bool {
 			}
 			nx := x + dx
 			ny := y + dy
-			delta := colorDelta(c, a.At(nx, ny), true)
+			fastAt(&nc, a, nx, ny)
+			delta := colorDelta(&c, &nc, true)
 
 			switch {
 			case delta == 0:
@@ -174,23 +186,24 @@ func isAntiAliased(a, b image.Image, x, y int) bool {
 	return (hasManySiblings(a, minX, minY) && hasManySiblings(b, minX, minY)) || (hasManySiblings(a, maxX, maxY) && hasManySiblings(b, maxX, maxY))
 }
 
-func hasManySiblings(img image.Image, x, y int) bool {
+func hasManySiblings(img *image.NRGBA, x, y int) bool {
 	if r := img.Bounds(); onEdge(r, x, y) {
 		return false
 	}
 
+	var c, nc color.RGBA
 	zeroes := 0
-	r, g, b, a := img.At(x, y).RGBA()
+	fastAt(&c, img, x, y)
+
 	for dy := -1; dy <= 1; dy++ {
 		for dx := -1; dx <= 1; dx++ {
 			if dy == 0 && dx == 0 {
 				continue
 			}
-
 			nx := x + dx
 			ny := y + dy
-			nr, ng, nb, na := img.At(nx, ny).RGBA()
-			if r == nr && g == ng && b == nb && a == na {
+			fastAt(&nc, img, nx, ny)
+			if c.R == nc.R && c.G == nc.G && c.B == nc.B && c.A == nc.A {
 				zeroes++
 			}
 			if zeroes > 2 {
@@ -203,4 +216,13 @@ func hasManySiblings(img image.Image, x, y int) bool {
 
 func onEdge(r image.Rectangle, x, y int) bool {
 	return x == r.Min.X || x == r.Max.X-1 || y == r.Min.Y || y == r.Max.Y-1
+}
+
+func fastAt(dest *color.RGBA, src *image.NRGBA, x, y int) {
+	offset := src.PixOffset(x, y)
+	p := src.Pix[offset:offset+4]
+	dest.R = p[0]
+	dest.G = p[1]
+	dest.B = p[2]
+	dest.A = p[3]
 }
